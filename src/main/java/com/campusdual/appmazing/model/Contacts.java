9ffbb579008959f8cb4f6d4a44995fd4c64f1ba1
Antía <antiagarrido2.0@gmail.com>
2023-09-26 12:00:15 +0200

    add class Contacts

---
+++ src/main/java/com/campusdual/appmazing/model/Contacts.java
@@ -0,0 +1,76 @@
+package com.campusdual.appmazing.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CONTACTS")
+
+
+public class Contacts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+
+    private String surname;
+    @Column
+
+    private String second_surname;
+    @Column
+
+    private String phone;
+    @Column
+
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSecond_surname() {
+        return second_surname;
+    }
+
+    public void setSecond_surname(String second_surname) {
+        this.second_surname = second_surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
