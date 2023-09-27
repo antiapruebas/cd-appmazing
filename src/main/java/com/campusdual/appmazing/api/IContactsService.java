07b897bee27d17efbf097805477590c323bc2ae9
Antía <antiagarrido2.0@gmail.com>
2023-09-27 12:07:18 +0200

    Services

---
+++ src/main/java/com/campusdual/appmazing/api/IContactsService.java
@@ -0,0 +1,16 @@
+package com.campusdual.appmazing.api;
+
+import com.campusdual.appmazing.model.dto.ContactsDTO;
+
+
+import java.util.List;
+
+public interface IContactsService {
+    ContactsDTO queryContacts(ContactsDTO product);
+    List<ContactsDTO> queryAllContacts();
+
+    int insertProduct(ContactsDTO contact);
+    int updateContact(ContactsDTO contact);
+
+    int deleteContact(ContactsDTO contact);
+}
