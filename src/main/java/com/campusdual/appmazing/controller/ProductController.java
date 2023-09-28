64311249ac5fe98ca1785d51ac7824b1337ff15b
Antía <antiagarrido2.0@gmail.com>
2023-09-28 10:44:37 +0200

    Product Controller

--- src/main/java/com/campusdual/appmazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appmazing/controller/ProductController.java
@@ -1,16 +1,35 @@
 package com.campusdual.appmazing.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.campusdual.appmazing.api.IProductService;
+import com.campusdual.appmazing.model.dto.ProductsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/products")
 public class ProductController {
 
+    @Autowired
+    IProductService productService;
+
     @GetMapping
     public String testController() {
     return "Product controller works!";
+    }
+
+    @PostMapping
+    public String testController(@RequestBody String name){
+        return "Product controller works " + name + "!";
+    }
+
+    @GetMapping (value="/testMethod")
+    public String testControllerMethod() {
+        return  "ProductController method works!";
+    }
+
+    @PostMapping(value="/get")
+    public ProductsDTO queryProduct(@RequestBody ProductsDTO product){
+        return this.productService.queryProducts(product);
     }
 
 
