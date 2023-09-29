97f113301ab4abe39a6c56fa93a07cd229de2d12
Antía <antiagarrido2.0@gmail.com>
2023-09-29 09:15:35 +0200

    Product Controller

--- src/main/java/com/campusdual/appmazing/api/IContactsService.java
+++ src/main/java/com/campusdual/appmazing/api/IContactsService.java
@@ -6,7 +6,7 @@ import com.campusdual.appmazing.model.dto.ContactsDTO;
 import java.util.List;
 
 public interface IContactsService {
-    ContactsDTO queryContacts(ContactsDTO product);
+    ContactsDTO queryContacts(ContactsDTO contact);
     List<ContactsDTO> queryAllContacts();
 
     int insertContact(ContactsDTO contact);
